67d63244a5a2cc74d2d7c6c21ebf96857c063122
Joseph Ditton <jditton.atomic@gmail.com>
2021-01-25 13:21:51 -0700

    adds class 1-15

---
+++ BasicAndroidViews/app/src/main/java/com/usu/basicandroidviews/MainActivity.java
@@ -0,0 +1,45 @@
+package com.usu.basicandroidviews;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.AppCompatEditText;
+import androidx.appcompat.widget.AppCompatTextView;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.ViewFlipper;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        LinearLayout mainLayout = new LinearLayout(this);
+        mainLayout.setOrientation(LinearLayout.VERTICAL);
+
+        AppCompatTextView instructionsTextView = new AppCompatTextView(this);
+        instructionsTextView.setText("Please enter your name");
+
+        AppCompatEditText nameEditText = new AppCompatEditText(this);
+        nameEditText.setHint("Your name here");
+
+        AppCompatButton displayMessageButton = new AppCompatButton(this);
+        displayMessageButton.setText("Display Message");
+
+        displayMessageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        mainLayout.addView(instructionsTextView);
+        mainLayout.addView(nameEditText);
+        mainLayout.addView(displayMessageButton);
+        setContentView(mainLayout);
+    }
+}
